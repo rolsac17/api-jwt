4bf2f86c569e69e2de1f8d8dc2e98dd97cf38d36
rolsac17 <46897486+rolsac17@users.noreply.github.com>
2022-10-27 16:12:17 -0600

    init repositrio
    api de login con jwt

---
+++ JsonWebTokenSingIn/JsonWebTokenSingIn/Controllers/UsuarioController.cs
@@ -0,0 +1,72 @@
+﻿using JsonWebTokenSingIn.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JsonWebTokenSingIn.Controllers
+{
+    [ApiController]
+    [Route("usuario")]
+    public class UsuarioController : ControllerBase
+    {
+        public IConfiguration _configuration;
+        public UsuarioController(IConfiguration configuration) 
+        { 
+            _configuration = configuration; 
+        }
+        [HttpPost]
+        [Route("login")]
+        public dynamic InicioSesion([FromBody] Object optData)
+        {
+            var data = JsonConvert.DeserializeObject<dynamic>(optData.ToString());
+
+            string user = data.usuario.ToString();
+            string password = data.password.ToString();
+
+            Usuario usuario = Usuario.DB().Where(x => x.usuario == user && x.password == password).FirstOrDefault();
+
+            if(usuario == null)
+            {
+                return new
+                {
+                    success = false,
+                    message = "Credenciales invalidas",
+                    result = ""
+                };
+            }
+
+            var jwt = _configuration.GetSection("Jwt").Get<Jwt>();
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, jwt.Subject),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim("id", usuario.idUsuario),
+                new Claim("usuario", usuario.usuario)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key));
+
+            var singIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                    jwt.Issuer,
+                    jwt.Audience,
+                    claims,
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: singIn
+                    );
+            return new
+            {
+                success = true,
+                message = "exito",
+                result = new JwtSecurityTokenHandler().WriteToken(token)
+            };
+        }
+
+    }
+}
---
+++ JsonWebTokenSingIn/JsonWebTokenSingIn/Usuario.cs
@@ -0,0 +1,41 @@
+﻿namespace JsonWebTokenSingIn
+{
+    public class Usuario
+    {
+        public string idUsuario { get; set; }
+        public string usuario { get; set; }
+
+        public string password { get; set; }
+        public string rol { get; set; }
+
+        public static List<Usuario> DB() 
+        {
+            var list = new List<Usuario>()
+            {
+                new Usuario
+                {
+                    idUsuario = "1",
+                    usuario = "Yarni",
+                    password = "1234",
+                    rol = "Admin"
+                },
+                 new Usuario
+                {
+                    idUsuario = "1",
+                    usuario = "Eduardo",
+                    password = "1234",
+                    rol = "Admin"
+                },
+                  new Usuario
+                {
+                    idUsuario = "1",
+                    usuario = "Rolando",
+                    password = "1234",
+                    rol = "Admin"
+                }
+            };
+
+            return list;
+        }
+    }
+}
